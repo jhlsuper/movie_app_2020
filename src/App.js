460903becc988bc1123fc4d02f0bff027c920273
Jeff Lee <jhlsuper@gmail.com>
2020-08-17 03:20:26 +0900

    movie component

--- src/App.js
+++ src/App.js
@@ -2,24 +2,21 @@ import React from 'react';
 import PropTypes from "prop-types";
 
 class App extends React.Component{
-  state ={
-    count: 0
+  state = {
+    isLoading: true
+    movies: []
   };
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({ isLoading: false});
 
-  add = () => {  //이것들은 react 코드가 아니라 자바스크립트 코드
-    this.setState(current => ({count: current.count +1}));
-  };
-  minus = () =>{
-    this.setState(current=>({count: current.count -1}));
-
-  };
+    }, 6000);  //6000단위의 시간 이후에 we are ready 로 만든다. 
+  }
 
   render() {  //react는 자동적으로 class 컴포넌트의 랜더 meothod를 자동적으로 실행한다.
-    return <div>
-      <h1>The number is {this.state.count}</h1>
-      <button onClick={this.add}>Add</button> 
-      <button onClick={this.minus}>Minus</button>
-      </div>
+    const {isLoading} = this.state;
+    return <div>{isLoading ? "Loading": "We are ready"}</div>
+      
   }
 }
 
