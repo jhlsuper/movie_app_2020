00fe5d21efd54e70f162c6c1a0d524b31197f5f0
Jeff Lee <jhlsuper@gmail.com>
2020-08-18 02:08:37 +0900

    4-1 done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import React from 'react';
+import React from "react";
 import PropTypes from "prop-types";
-
-class App extends React.Component{
+import axios from "axios";
+import Movie from "./Movie";
+class App extends React.Component {
   state = {
     isLoading: true,
-    movies: []
+    movies: [],
   };
-  componentDidMount(){
-    setTimeout(() => {
-      this.setState({ isLoading: false});
 
-    }, 6000);  //6000단위의 시간 이후에 we are ready 로 만든다. 
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies,json?sort_by=rating"
+    ); //data안에있는 data 의 movies
+    this.setState({ movies, isLoading: false }); //movies를 setstate해주면 we are ready로 바꿈
+  };
+  componentDidMount() {
+    this.getMovies();
   }
 
-  render() {  //react는 자동적으로 class 컴포넌트의 랜더 meothod를 자동적으로 실행한다.
-    const {isLoading} = this.state;
-    return <div>{isLoading ? "Loading": "We are ready"}</div>
-      
+  render() {
+    //react는 자동적으로 class 컴포넌트의 랜더 meothod를 자동적으로 실행한다.
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+      </div>
+    );
   }
 }
 
